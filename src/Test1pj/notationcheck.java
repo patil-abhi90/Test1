b3a9e9810310ace6285c6127d5ff9453bb534b2e
Abhijeet <abhijeetpatil2590@gmail.com>
2023-08-03 18:37:15 +0530

    Invalid notation check

--- src/Test1pj/notationcheck.java
+++ src/Test1pj/notationcheck.java
@@ -5,16 +5,17 @@ public class notationcheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 10; 
-
+//pass
 		int int = 20; 
-
+//invalid variable declared
 		int goto = 30; 
-
+//invalid variable declared
 		int default = 40; 
-
+//invalid variable declared
 		int class = 50; 
-
+//invalid variable declared
 		int j = 70;
+//Pass
 	}
 
 }
