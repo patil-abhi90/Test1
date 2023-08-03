e3cf057b7b01ec92a600f4f9b01587f4ba7d7d42
Abhijeet <abhijeetpatil2590@gmail.com>
2023-08-03 18:32:18 +0530

    notationcheck

---
+++ src/Test1pj/notationcheck.java
@@ -0,0 +1,20 @@
+package Test1pj;
+
+public class notationcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10; 
+
+		int int = 20; 
+
+		int goto = 30; 
+
+		int default = 40; 
+
+		int class = 50; 
+
+		int j = 70;
+	}
+
+}
